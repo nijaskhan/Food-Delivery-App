2808ba0464a6cbe79cc3c1da3d8c11575cb9ecff
nijaskhan <nijaskhan93@gmail.com>
2023-05-07 03:03:44 +0530

    updated bugs

--- src/pages/login.js
+++ src/pages/login.js
@@ -29,7 +29,7 @@ const Login = () => {
                                             </div>
                                             <div className="form-outline mb-4">
                                                 <label className="form-label" for="form2Example22" style={{ color: '#545563' }}>Password</label>
-                                                <input type="password" id="form2Example22" className="form-control" />
+                                                <input type="password" id="form2Example22" placeholder='password' className="form-control" />
                                             </div>
                                             <div className="form-outline mb-4">
                                                 <input type="checkbox" style={{ color: '#545563' }} id="form2Example22" />
