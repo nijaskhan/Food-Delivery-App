e94f545e4e8953224168352f4accf5695ac11f50
nijaskhan <nijaskhan93@gmail.com>
2023-05-07 21:22:07 +0530

    responsive check pending, banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,33 +19,41 @@ export default function Home() {
     <>
       <Head>
         <title>Food Delivery | Home</title>
-        <meta name="keywords" content="foodDelivery"/>
+        <meta name="keywords" content="foodDelivery" />
       </Head>
       <section className="container">
-        <div className='row justify-content-center'>
-          <div className="col-md-6 rounded-4 box-shadow" style={{ backgroundColor: '#F3F4FF', width: '48%' }}>  {/* box shadow def in global.css */}
-            <div className="d-flex">
-              <Image src='/banner21.png' className='mt-5' alt='banner' width={353} height={240} />
-              <div className='d-block mt-5'>
-                <p className='bannerName'>All deserts</p>
-                <h1 className='bannerOff' style={{ color: '#4E60FF' }}>20% OFF</h1>
-                <div className='align-items-end'>
-                  <div class="d-flex bannerDesc align-items-end" style={{height: '5.5rem'}}>
-                    <p class="m-0">Deserty</p>
+        <div className='row justify-content-center' >
+          <div className="col-md-6 col-lg-6 col-12 rounded-4 box-shadow my-3 mx-3 mb-md-0 card" style={{ backgroundColor: '#F3F4FF', border: 'none', width: '39rem'}}>
+            <div className="row d-flex">
+              <div className='col-lg-6 col-12 col-md-12'>
+                <Image src='/banner21.png' className='mt-5 card-img-left' alt='banner' width={353} height={240} />
+              </div>
+              <div className="col-12 col-md-12 col-lg-6 order-first order-md-last">
+                <div className='d-block mt-3 ms-5 card-body'>
+                  <p className='bannerName'>All deserts</p>
+                  <h1 className='bannerOff mt-lg-5' style={{ color: '#4E60FF' }}>20% OFF</h1>
+                  <div className='align-items-end'>
+                    <div class="d-flex bannerDesc align-items-end" style={{ height: '5.5rem' }}>
+                      <p class="m-0">Deserty</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-md-6 rounded-4 ms-5" style={{ backgroundColor: '#FFF3ED', width: '48%' }}>
-            <div className="d-flex">
-              <Image src='/banner22.png' className='mt-5' alt='banner' width={353} height={240} />
-              <div className='d-block mt-5'>
-                <p className='bannerName'>All deserts</p>
-                <h1 className='bannerOff text-danger'>50% OFF</h1>
-                <div className='align-items-end'>
-                  <div class="d-flex bannerDesc align-items-end" style={{height: '5.5rem'}}>
-                    <p class="m-0">Fooddies</p>
+          <div className="col-md-6 col-lg-6 col-12 rounded-4 box-shadow my-3 mx-2 mb-md-0 card" style={{ backgroundColor: '#F3F4FF', border: 'none', width: '39rem'}}>
+            <div className="row d-flex">
+              <div className='col-lg-6 col-12 col-md-12'>
+                <Image src='/banner22.png' className='mt-5 card-img-left' alt='banner' width={353} height={240} />
+              </div>
+              <div className="col-12 col-md-12 col-lg-6 order-first order-md-last">
+                <div className='d-block mt-3 ms-5 card-body'>
+                  <p className='bannerName'>Big Burgers</p>
+                  <h1 className='bannerOff mt-lg-5' style={{ color: '#FD6D22' }}>50% OFF</h1>
+                  <div className='align-items-end'>
+                    <div class="d-flex bannerDesc align-items-end" style={{ height: '5.5rem' }}>
+                      <p class="m-0">Fooddies</p>
+                    </div>
                   </div>
                 </div>
               </div>
